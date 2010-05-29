46909bedfe1195a4da3306d498f9a9e644f3e806
masi <masi@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-05-29 15:19:26 +0000

    fix dependencies
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ballroom_dancing/branches/extbase-1.1@33697 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'category' => 'example',
 	'shy' => 0,
 	'version' => '1.1.0',
-	'dependencies' => 'extbase',
+	'dependencies' => 'extbase,fluid',
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
@@ -35,8 +35,9 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.2.0-0.0.0',
-			'typo3' => '4.3.dev-4.3.99',
-			'extbase' => '1.1.0',
+			'typo3' => '4.3.0',
+			'extbase' => '1.1.0-1.1.99',
+			'fluid' => '1.1.0',
 		),
 		'conflicts' => array(
 		),
